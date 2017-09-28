1f0084462bc3069df40d3def3b9679de7feacdf0
applandtea <253421981@qq.com>
2017-09-28 10:03:53 +0800

    keyword 更改前备份

--- functions.php
+++ functions.php
@@ -1,63 +1,102 @@
-<?php
+<?php
+
 
-/*输出获取的指定信息*/
-function setInfoLink($webName,$source,$sourceWebRoot,$cutSource,$keyWord){
-	/*获取原网址源文件*/
-	$ch = curl_init();  
-	$timeout = 10; // set to zero for no timeout  
-	curl_setopt ($ch, CURLOPT_URL,$source);  
-	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);   
-	curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');  
-	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);  
-	$html = curl_exec($ch);  
+/*读取页面*/
+function readPage($webName,$sourceWeb,$sourceWebRoot,$sourceWebIndexName,$sourceWebIndexFormat,$cutSource,$keyWord,$page){	
+	for($sourceNumber=0;$sourceNumber<count($sourceWeb);$sourceNumber++){
+		if($page[$sourceNumber]===1){   //只读取一页
+			outputLink($webName[$sourceNumber],$sourceWeb[$sourceNumber],$sourceWebRoot[$sourceNumber],$cutSource[$sourceNumber],$keyWord[$sourceNumber]);
+		}
+		else{						  //读取多页
+			for($pageNumber=0;$pageNumber<$page[$sourceNumber];$pageNumber++){
+				if($pageNumber===0){
+					$sourceWebTemp[$pageNumber] = $sourceWebRoot[$sourceNumber].$sourceWebIndexName[$sourceNumber].'.'.$sourceWebIndexFormat[$sourceNumber];
+				}
+				else{
+					$sourceWebTemp[$pageNumber] = $sourceWebRoot[$sourceNumber].$sourceWebIndexName[$sourceNumber].'_'.$pageNumber.'.'.$sourceWebIndexFormat[$sourceNumber];
+				}
+				outputLink($webName[$sourceNumber],$sourceWebTemp[$pageNumber],$sourceWebRoot[$sourceNumber],$cutSource[$sourceNumber],$keyWord[$sourceNumber]);
+			}
+		}
+	}
+		//outputLink($webName[0],$sourceWeb[0],$cutSource[0],$keyWord);
+		//outputLink($webName[1],$sourceWeb[1],$cutSource[1],$keyWord);
+		/*
+		$html1 = file_get_contents($sourceWeb[1]);
+		$html1 = changeToUtf($html1);
+		preg_match_all($cutSource[1],$html1,$result);
+		//var_dump($result);
+		echo $result[0][0];*/
 
-	/*编码变为utf-8*/
-	$html = changeToUtf($html);
-
-	/*截取指定内容*/
-	preg_match_all($cutSource,$html,$result);
-
-	/*输出包含指定关键字的标题*/
-    for($j=0;$j<count($result[0]);$j++)
-    {
-		$find = $keyWord;
-		$find = changeToUtf($find);	
-		/*匹配关键字*/
-		if(strpos($result[0][$j],$find)){	
-			/*获取链接文字内容*/
-			$title =  get_tag_title($result[0][$j],'a');   
-			/*获取相对链接地址*/
-			$link = get_tag_link($result[0][$j],'a','href');
-			print '<li style="list-style-type:decimal;border-bottom:1px dashed #eee;">'.'<span>'.'</span>'.'<a href="'.$sourceWebRoot.$link[0].'" target="_blank" title="'.$title[0].'">'.$title[0].'</a>'.'('.changeToUtf($webName.'发布').')'.'</li>'; 
-		}	
-    }
 }
 
-/*转换中文编码格式为UTF-8*/
-function changeToUtf($sourceString){
-    $charset = mb_detect_encoding($sourceString,array('UTF-8','GBK','GB2312')); 
-    $charset = strtolower($charset); 
-    if('cp936' == $charset){ 
-        $charset='GBK'; 
-    } 
-    if("utf-8" != $charset){ 
-        $sourceString = iconv($charset,"UTF-8//IGNORE",$sourceString); 
-		return $sourceString;
-    } 
-}
-
-/*截取筛选后内容中的链接文字*/
-function get_tag_title($result,$tag){
-	$regex = "/<$tag.*?>(.*?)<\/$tag>/is";
-	preg_match_all($regex,$result,$matches,PREG_PATTERN_ORDER);
-	return $matches[1];
-}
-
-/*截取筛选后内容中的链接地址*/
-function get_tag_link($result,$tag,$attr){
-	$regex = "/<$tag\s$attr=\'(.*?)\'.*?>.*?<\/$tag>/is";
-	preg_match_all($regex,$result,$matches,PREG_PATTERN_ORDER);
-	return $matches[1];
-}
-
+/*输出获取的指定信息*/
+function outputLink($webName,$source,$sourceWebRoot,$cutSource,$keyWord){
+	/*获取原网址源文件*/
+	$ch = curl_init();  
+	$timeout = 10; // set to zero for no timeout  
+	curl_setopt ($ch, CURLOPT_URL,$source);  
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);   
+	curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');  
+	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);  
+	$html = curl_exec($ch);  
+
+	/*编码变为utf-8*/
+	$html = changeToUtf($html);
+
+	/*截取指定内容*/
+	preg_match_all($cutSource,$html,$result);
+
+	/*输出包含指定关键字的标题*/
+    for($j=0;$j<count($result[0]);$j++)
+    {
+		$find = $keyWord;
+		$find = changeToUtf($find);	
+		/*匹配关键字*/
+		if(strpos($result[0][$j],$find)){	
+			/*获取链接文字内容*/
+			$title =  get_tag_title($result[0][$j],'a');   
+			/*获取相对链接地址*/
+			$link = get_tag_link($result[0][$j],'a','href');
+			$linkDate = get_tag_date($result[0][$j]);
+			
+			print '<li style="list-style-type:decimal;border-bottom:1px dashed #eee;">'.'<span>'.$linkDate[0].'</span>&nbsp;&nbsp;'.'<a href="'.$sourceWebRoot.$link[0].'" target="_blank" title="'.$title[0].'">'.$title[0].'</a>'.'&nbsp;&nbsp;('.changeToUtf($webName.'发布').')'.'</li>'; 
+		}	
+    }
+}
+
+/*转换中文编码格式为UTF-8*/
+function changeToUtf($sourceString){
+    $charset = mb_detect_encoding($sourceString,array('UTF-8','GBK','GB2312')); 
+    $charset = strtolower($charset); 
+    if('cp936' == $charset){ 
+        $charset='GBK'; 
+    } 
+    if("utf-8" != $charset){ 
+        $sourceString = iconv($charset,"UTF-8//IGNORE",$sourceString); 
+		return $sourceString;
+    } 
+}
+
+/*截取筛选后内容中的链接文字*/
+function get_tag_title($result,$tag){
+	$regex = "/<$tag.*?>(.*?)<\/$tag>/is";
+	preg_match_all($regex,$result,$matches,PREG_PATTERN_ORDER);
+	return $matches[1];
+}
+
+/*截取筛选后内容中的链接地址*/
+function get_tag_link($result,$tag,$attr){
+	$regex = "/<$tag\s$attr=\'(.*?)\'.*?>.*?<\/$tag>/is";
+	preg_match_all($regex,$result,$matches,PREG_PATTERN_ORDER);
+	return $matches[1];
+}
+
+/*截取筛选后date*/
+function get_tag_date($result){
+	$regex = "/\d{4}-\d{2}-\d{2}/is";
+	preg_match_all($regex,$result,$matches,PREG_PATTERN_ORDER);
+	return $matches[0];
+}
+
 ?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8"  />
+	<!--<script src="scripts/news.js"></script>-->
+</head>
+<body>
+<?php include 'init.php';?>
+<?php include 'functions.php';?>
+
+<form action="#" method="POST" >
+<fieldset style="width:90%;margin:10px auto;padding:10px;">
+<legend>optionSet</legend>
+<?php echo changeToUtf('1.À´Ô´ÍøÕ¾  2.¹Ø¼ü×Ö');?>
+<input type="reset"/> <input type="submit">
+</fieldset>
+
+</form>
+
+<div style="padding:10px;margin:0 auto;width:90%;border:1px solid #000;">
+	<h2><?php echo changeToUtf($keyWord[0]); ?></h2>
+	<ul>
+		<?php
+			readPage($webName,$sourceWeb,$sourceWebRoot,$sourceWebIndexName,$sourceWebIndexFormat,$cutSource,$keyWord,$page);
+		?> 
+	</ul>
+</div>
+</body>
+</html>
